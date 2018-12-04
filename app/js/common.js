d8483af9e1a841bb8e11fdf0e73bf4d8463c5992
DenisSoloviev <dionisio14solo14@gmail.com>
2018-12-04 18:09:16 +0200

    page coming soon and begin two section + fix work scripts

---
+++ app/js/common.js
@@ -0,0 +1,39 @@
+$(function() {
+	
+	/* 
+	*
+	*	COMING SOON PAGE
+	*	
+	*/
+	
+	var $comingSoon = $('.coming-soon'),
+		$letterLeft = $('.coming-soon__letter-left'),
+		$letterRight = $('.coming-soon__letter-right'),
+		$circle = $comingSoon.find('.animation-circle'),
+		$bg = $comingSoon.find('.bg');
+		
+	
+	
+	$letterLeft.show().addClass('animated slideInLeft delay-2s');
+	$letterRight.show().addClass('animated slideInRight delay-2s');
+	
+//	$comingSoon.delay(3000).animate({
+//		"background-position-y": "0"
+//	}, 1000);
+//	
+	$bg.delay(4000).animate({
+		opacity: .1
+	}, 1200).addClass('animated slideInDown delay-2s');
+	
+	$circle.delay(6000).animate({
+		opacity: 1
+	}, 1000, function() {
+		$(this).find('img').animate({
+			opacity: 1
+		}, 500);
+		$(this).find('p').delay(300).animate({
+			opacity: 1
+		}, 400);
+	});
+	
+});
